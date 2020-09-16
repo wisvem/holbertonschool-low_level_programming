74d897a583608ca40ed277572f13446f25740aec
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-16 21:06:57 +0000

    Fix task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -6,14 +6,15 @@ include <stdio.h>
  * Return: Always 0 (Success)
  */
 int main() {
-    int intType;
-    float floatType;
-    double doubleType;
-    char charType;
-    printf("Size of int: %ld bytes\n", sizeof(intType));
-    printf("Size of float: %ld bytes\n", sizeof(floatType));
-    printf("Size of double: %ld bytes\n", sizeof(doubleType));
-    printf("Size of char: %ld byte\n", sizeof(charType));
-    
-    return 0;
+	int intType;
+	long longIntType;
+	double  longLongType;
+	char charType;
+	float floatType;
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longLongType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+	return 0;
 }
