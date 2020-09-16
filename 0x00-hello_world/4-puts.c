629c27dc4e57f4f45c44a444a4785312f32ff44d
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-16 18:49:42 +0000

    Fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main(void)
+
+/**
+ * main - Entry point 
+ * Prints a string
+ *
+ * Return: Always 0 (Success)
+ */
 {
-puts( "\"Programming is like building a multilingual puzzle" );
-return(0);
+ puts( "\"Programming is like building a multilingual puzzle" );
+ return(0);
 }
