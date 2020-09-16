8ae2c73c9aaba2ce9ae0a684fb61da303c225d4e
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-16 22:08:31 +0000

    Fix task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * print a string without printf() or puts()
  * Return: Always 1 (sucess)
  */
-int main()
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
