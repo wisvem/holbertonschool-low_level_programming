c8774a0366b5ba970b1123736b413df95e22ab04
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-17 23:54:50 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print base 10 numbers
+ * Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num + 48);
+	putchar('\n');
+	return (0);
+}
