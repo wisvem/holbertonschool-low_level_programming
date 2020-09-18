2a3a8eb9a4e494d03a067ad339651d13a705cdd6
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-18 22:58:40 +0000

    task 13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 	{
 		for (num2 = 1; num2 <= 99; num2++)
 		{
-			if ((num2 > num) || (num == 0 && j != 0))
+			if ((num2 > num) || (num == 0 && num2 != 0))
 			{
 				um = (num % 10) % 10;
 				c = num % 10;
