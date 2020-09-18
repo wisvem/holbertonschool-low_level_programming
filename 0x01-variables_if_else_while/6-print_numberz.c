dddafd81d3390b106cd2144c9aaebb1f9d06c488
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-18 00:17:24 +0000

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@
 **/
 int main(void)
 {
-	char num;
+	int num;
 
-	for (num = '0'; num <= '9'; num++)
-		putchar(num);
+	for (num = 0; num <= 9; num++)
+		putchar(num + 48);
 	putchar('\n');
 	return (0);
 }
