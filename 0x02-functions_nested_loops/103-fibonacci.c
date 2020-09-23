283314da80975e43ee920f7319ce9f2ea34f1582
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-23 04:41:44 +0000

    Task 15 Fibo

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - fibonacci
+* Return: 0 success
+**/
+int main(void)
+{
+	long a = 1;
+	long b = 2;
+	int i = 0;
+	long c;
+
+	while (a <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (a % 2 == 0)
+		{
+			i = i + a;
+		}
+	}
+	return (0);
+}
