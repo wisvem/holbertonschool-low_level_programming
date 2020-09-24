72c3926865e4502b69f7eebe48898a7c11a35551
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-24 04:30:37 +0000

    Task 16 thanks to Soren and Alejandro

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,25 +6,42 @@
 **/
 int main(void)
 {
-	unsigned long a = 1;
-	unsigned long b = 2;
 	int i;
-	unsigned long c;
+	unsigned long a = 0;
+	unsigned long b = 1;
+	unsigned long next;
+	unsigned long part1;
+	unsigned long part2;
+	unsigned long a2;
+	unsigned long b2;
 
-	printf("%lu, ", a);
-	for (i = 1; i <= 97; ++i)
+	for (i = 0; i < 91; i++)
 	{
-		c = a + b;
+		next = a + b;
 		a = b;
-		b = c;
-		if (i == 97)
-		{
-			printf("%lu\n", a);
-		}
-		else
-		{
-			printf("%lu, ", a);
-		}
+		b = next;
+		printf("%ld", next);
+		printf(", ");
 	}
+	b = a % 10000; /* Last four digits of fibo number 91*/
+	a = a / 10000; /* fibo number 91 without four last digits */
+	a2 = next / 10000; /* */
+	b2 = next % 10000;
+	for (i = 0; i < 6; i++)
+	{
+		part2 = (b + b2) % 10000;
+		if ((b + b2) > 9999)
+			part1 = a + a2 + 1;
+		else
+			part1 = a + a2;
+		a = a2;
+		b = b2;
+		a2 = part1;
+		b2 = part2;
+		printf("%lu%lu", part1, part2);
+		if (i != 5)
+			printf(", ");
+	}
+	printf("\n");
 	return (0);
 }
