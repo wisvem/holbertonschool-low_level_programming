39dbfaf45a26edc76591ebc2d0ac82dc5a424473
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-24 23:52:18 +0000

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,12 +7,20 @@ void main(void)
 {
 	long n = 612852475143;
 	long pf;
+	int i;
 
-
-	while(n)
+	for (i = 2; i <= n; i++)
 	{
-		if (n % 2)
+		if (n % i == 0)
+		{
+			n = n / i;
+			i--;
+		}
+		if (n != 1)
+		{
+			pf = n;
+		}
 
 	}
-	printf("%ld", n);
+	printf("%ld\n", pf);
 }
