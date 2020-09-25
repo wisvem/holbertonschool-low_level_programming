c83a1336780cf3eeb6dd4ed56149d806ad8abd81
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-25 01:47:49 +0000

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,33 +1,19 @@
 #include "holberton.h"
 
 /**
- *print_number - prints the number given
- *@n: the number to be printed
- *Return: no returns
- */
-
+* print_number - print integers
+* @n: integer to print
+**/
 void print_number(int n)
 {
-	int i, tmp;
+	int i;
+	int res;
 
-	i = 1;
-	if (n < 0)
+	for (i = 1; i <= n;)
 	{
-		_putchar(45);
-		if (n == -2147483648)
-		{
-			_putchar('0' + 2);
-			n = n % 1000000000;
-		}
-		n = -n;
+		res = n % 10;
+		_putchar('0' + res);
+		n = n / 10;
+
 	}
-	tmp = n;
-	i = 1;
-	while (tmp / 10 != 0)
-	{
-		i = i * 10;
-		tmp = tmp / 10;
-	}
-	for (; i > 0; i = i / 10)
-		_putchar('0' + ((n / i) % 10));
 }
