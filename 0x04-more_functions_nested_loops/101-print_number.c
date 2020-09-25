23f4c21bb9d6e68c384203d36690a999b12ff904
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-25 02:02:45 +0000

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,10 +8,10 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 	if (n / 10)
 		print_number(n / 10);
-	putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
