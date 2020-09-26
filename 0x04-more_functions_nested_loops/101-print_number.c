0619faa4831aff3e80e2d21d81f5f7d6264fcf50
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-26 02:30:29 +0000

    Task 12 finally./test !

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,13 +6,43 @@
 **/
 void print_number(int n)
 {
-	if (n < 0)
+	int i = 1, j, divi = 1;
+	int bck = n;
+
+	if (n >= 0 && n <= 9)
 	{
-		_putchar('-');
+		_putchar('0' + n);
+	}
+	else if (n < 0 && n >= -9)
+	{
 		n = -n;
+		_putchar('-');
+		_putchar('0' + n);
 	}
-	else if (n == 0)
+	else
 	{
-		_putchar('0');
+		if (n <= -10)
+		{
+			n = -n;
+			_putchar('-');
+			bck = n;
+		}
+		while (n > 10)
+		{
+			n = n / 10;
+			i++;
+		}
+		for (j = i; j > 1; j--)
+		{
+			divi = divi * 10;
+		}
+		for (j = 1; j <= i; j++)
+		{
+			n = bck / divi;
+			bck = bck - (n * divi);
+			divi = divi / 10;
+			_putchar('0' + n);
+		}
 	}
+	_putchar('\n');
 }
