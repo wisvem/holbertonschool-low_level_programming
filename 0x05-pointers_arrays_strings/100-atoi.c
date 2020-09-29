455684a1aa974c9b28e9a624decc71797691f1a0
Wiston Venera Macias <2170@holbertonschool.com>
2020-09-29 22:39:18 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,32 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
-* _atoi - convets string to int
+* _atoi - string to int
 * @s: string
-* Return: intenger
+* Return: int
 **/
 int _atoi(char *s)
 {
+	char *str;
+	int i = 0, j = 0;
+	char *aux;
+
+	aux = str;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 48 && str[i] <= 57)
+		{
+			printf("Si %c\n", str[i]);
+			*(aux + j) = str[i];
+			j++;
+		}
+		else
+		{
+			printf("No %c\n", str[i]);
+		}
+		i++;
+	}
+	printf("%d\n", i);
 	return (0);
+
 }
