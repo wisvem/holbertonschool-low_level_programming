f3005e1ec3b3d20af71c1a97e5afe1435a56b1d3
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-01 18:37:04 +0000

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int sizedest = 0, i = 0;
 
-	while (dest[sizedest] != '\0')
-	{
-		sizedest++;
-	}
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++
 	}
 	return (dest);
 }
