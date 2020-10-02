b88259a085397a6c6dcf5d79cb45dd36a6a35cc9
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-02 02:08:03 +0000

    task 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* rot13 -  encodes a string using rot13
+* @str: string to encode
+* Return: string encoded
+*/
+char *rot13(char *str)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0, j;
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+			}
+		}
+		i++;
+	}
+	return (str);
+}
