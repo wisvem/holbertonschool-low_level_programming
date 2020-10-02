ab8ea1a7f39fe91b3187a1a21be0273bd91dc089
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-02 04:23:38 +0000

    Task 11 and 12

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* infinite_add - adds two numbers
+* @n1: number 1
+* @n2: number 2
+* @r: result
+* @size_r: size
+* Return: result
+**/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+	return (r);
+}
---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_line - prints a s bytes of a buffer
+* @c: buffer to print
+* @s: bytes of buffer to print
+* @l: line of buffer to print
+*
+* Return: void
+**/
+
+void print_line(char *c, int s, int l)
+{
+}
