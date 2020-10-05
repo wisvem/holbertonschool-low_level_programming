093e140299fefdd47d010ca4c49853b4ad284d3a
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-05 23:43:55 +0000

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* _strspn - gets the length of a prefix substring
+* @s: source
+* @accept: string to find
+* Return: bytes
+**/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, cont = 0, find = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				find = 1;
+				break;
+			}
+			j++;
+		}
+		if (find == 1)
+		{
+			cont++;
+		}
+		i++;
+	}
+	return (cont);
+}
