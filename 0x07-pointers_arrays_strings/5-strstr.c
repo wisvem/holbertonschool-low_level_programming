3577c594185bf99cb3dc030458156eb35eb7b0ba
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-05 20:51:05 +0000

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+* _strstr - find a string inside a string
+* @haystack:  source string
+* @needle: string to find
+* Return: pointer to first occurence else null
+**/
+char *_strstr(char *haystack, char *needle)
+{
+	int size_h = 0, size_n = 0, i, j, sw = 0;
+	char *p;
+
+	while (haystack[size_h] != '\0')
+	{
+		size_h++;
+	}
+	while (needle[size_n] != '\0')
+	{
+		size_n++;
+	}
+	for (i = 0; i < size_h; i++)
+	{
+		if (haystack[i] == needle[j])
+		{
+			j++;
+		}
+		else
+		{
+			j = 0;
+		}
+	}
+	if (j == size_n)
+	{
+		p = (haystack +  (size_h - j));
+		return (p);
+	}
+	return (p);
+}
