d4b9d00d9718e9af33a635501f2d7cc0dd48d20b
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-05 19:36:30 +0000

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+* _strchr - locate a character in  a string
+* @s: source string
+* @c: character
+* Return: return a pointer to the first ocur
+**/
+char *_strchr(char *s, char c);
+{
+	int i = 0, sw = 0;
+	char *p;
+
+	p = NULL;
+	while (s[i] != '\0' && sw == 0}
+	{
+		if (s[i] == c)
+		{
+			p = s + i;
+			return (p);
+		}
+	}
+	return (p);
+}
