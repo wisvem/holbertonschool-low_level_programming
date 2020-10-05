5f3095330a83ca9659666d0f51d33669735c9c77
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-05 22:25:53 +0000

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,20 +6,16 @@
 **/
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-	char *p = '\0';
+	int i = 0, j;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-		{
-			p = s + i;
-			return (p);
-		}
-		else if (s[i] == '\0')
-		{
-			return ('\0');
-		}
 		i++;
 	}
+	for (j = 0; j <= i; j++)
+	{
+		if (s[j] == c)
+			return (s + j);
+	}
+	return ('\0');
 }
