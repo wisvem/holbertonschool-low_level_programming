860f55f256839307b9a6d15553d44fd5189a39ab
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-06 03:00:02 +0000

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include
+/**
+* print_chessboard - prints a chessboard
+* @a: chessboard
+**/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
