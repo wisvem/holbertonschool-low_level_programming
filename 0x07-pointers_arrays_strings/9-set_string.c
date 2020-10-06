a25d09e6f025cdf1e23757d2e6891defe2fec535
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-06 03:40:09 +0000

    Task 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* set_string - sets the value of a pointer to a char
+* @s: char pointer to pointer
+* @to: char pointer
+**/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
