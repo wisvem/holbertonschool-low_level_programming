9355acba36eb08c59bc2dafd52ab8119fec0ef1e
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-08 00:20:05 +0000

    Task 6-7

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+* prime - auxiliar
+* @x: the number
+* @y: the auxiliar
+* Return: 1 prime
+**/
+int prime(int x, int y)
+{
+	if (x == y)
+	{
+		return (1);
+	}
+	if (x % y == 0)
+	{
+		return (0);
+	}
+	return (prime(x, y + 1));
+}
+/**
+* is_prime_number - is prime
+* @n: int to be evaluated
+* Return: 1 for prime otherwise 0
+**/
+int is_prime_number(int n)
+{
+	int es = prime(n, 2);
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	return (es);
+}
---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s: string
+* Return: lenght of @s
+**/
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+* palin - palindrome functio
+* @s: String to evaluate
+* @i: an auxiliar
+* @lenght: length of @s
+* Return: 1 if string is a palindrome and 0 if not.
+**/
+int palin(char *s, int i, int lenght)
+{
+	if (i == lenght)
+	{
+		return (1);
+	}
+	if (s[i] != s[lenght])
+	{
+		return (0);
+	}
+	if (i < lenght)
+	{
+		return (palin(s, i + 1, lenght - 1));
+	}
+	return (1);
+}
+/**
+* is_palindrome - checks if a string is palindrome
+* @s: String to be evaluated
+* Return: 1 for palindrome, 0 no palindrome.
+**/
+int is_palindrome(char *s)
+{
+	int es = palin(s, 0, (_strlen_recursion(s) - 1));
+
+	if (_strlen_recursion(s) == 0)
+	{
+		return (1);
+	}
+	return (es);
+}
