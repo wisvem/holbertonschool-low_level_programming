f2d0b5a3658d11c5d38f6e7257568c43bb45d5be
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-13 17:27:46 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,14 @@
 **/
 char *create_array(unsigned int size, char c)
 {
-	char *ptr = NULL;
+	char *ptr;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
