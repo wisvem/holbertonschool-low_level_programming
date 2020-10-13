e2479fd9ad9678961d2f49adcca4ed29313b2d2f
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-13 23:31:36 +0000

    Task 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,36 +1,48 @@
-#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * argstostr - concatenates all the arguments of your program
- * @ac: argc
- * @av: argv
- * Return: pointer to new string
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: resultating string
  */
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, len = 0, k = 0;
-	char *ptr;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+int i, j, k = 0, countComplete = 0;
+char *s;
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			len++;
-	}
-	ptr = malloc(len + 1 * sizeof(char));
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++, k++)
-	{
-		for (j = 0; av[i][j]; j++, k++)
-		{
-			ptr[k] = av[i][j];
-		}
-		ptr[k] = '\n';
-	}
-	return (ptr);
+if (ac <= 0 || av == NULL)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+countComplete++;
+}
+countComplete++;
+}
+countComplete++;
+
+s = malloc(countComplete *sizeof(char));
+
+if (s == NULL)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+s[k] = av[i][j];
+k++;
+}
+s[k] = '\n';
+k++;
+}
+
+s[k] = '\0';
+return (s);
 }
