b6f969888a57bc9f95140c331b98148ea4c92c52
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-13 20:34:36 +0000

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,19 +8,22 @@
 **/
 char *_strdup(char *str)
 {
-	int i = 0, j;
+	unsigned int i = 0, j;
 	char *ptr;
 
 	if (str == NULL)
+	{
 		return (NULL);
-
-	ptr = malloc(sizeof(char) * i + 1);
+	}
 	while (str[i] != '\0')
-	i++;
-
+	{
+		i++;
+	}
+	ptr = malloc(sizeof(char) * (i + 1));
 	if (ptr == NULL)
+	{
 		return (NULL);
-
+	}
 	for (j = 0; j <= i; j++)
 	{
 		ptr[j] = str[j];
