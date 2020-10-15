bb08150b58afe0b1106c96ced78d98e5a47deb0d
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-15 19:56:08 +0000

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,11 +12,19 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i;
 
+/*	if (size == 0 || nmemb == 0)
+*	{
+*		return (NULL);
+*	}
+*/
 	ptr = malloc(nmemb * size);
 	if (ptr != NULL)
-		for (i = 0; nmemb * size; i++)
+	{
+		for (i = 0; i < nmemb * size; i++)
 		{
 			ptr[i] = 0;
 		}
-	return (void(ptr));
+		return (ptr);
+	}
+	return (NULL);
 }
