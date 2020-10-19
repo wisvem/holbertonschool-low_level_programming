2552962f952ae0e2c0e40add1d7fee794ab9547b
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-19 18:03:00 +0000

    Fix Task 1-4

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,5 @@
-#ifndef 1_PI_H_
-#define 1_PI_H_
+#ifndef PI
+#define PI
 
 #define PI 3.14159265359
 #endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 
-#ifndef 3_FUNCTION_LIKE_MACRO_H_
-#define 3_FUNCTION_LIKE_MACRO_H_
+#ifndef 3_FUNCTION_LIKE_MACRO
+#define 3_FUNCTION_LIKE_MACRO
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 #endif
