e975698d7152ec1385f9acd15fb968c5b44d3502
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-19 21:09:12 +0000

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* init_dog - Initialize dog structure
+* @d: dog strcture
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Return: Always 0.
+**/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+	return (0);
+}
