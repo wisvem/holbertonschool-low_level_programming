b6457cdf9cc37f4992c38506c98f7f316878dc2f
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-19 23:14:13 +0000

    Task 2 added nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,6 +17,6 @@ void print_dog(struct dog *d)
 	if((*d).owner == NULL)
 		(*d).owner = "(nil)";
 	printf("Name: %s\n", (*d).name);
-	printf("Age: %.1f\n", (*d).age);
+	printf("Age: %f\n", (*d).age);
 	printf("Owner: %s\n", (*d).owner);
 }
