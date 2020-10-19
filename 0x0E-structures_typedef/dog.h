16075be8ab609e31f895ae2ed879f01735e5e0c0
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-19 23:44:08 +0000

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
 * @age: Age of the dog
 * @owner: Owner of the dog
 **/
-typedef struct dog t_dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* _DOG_H_ */
