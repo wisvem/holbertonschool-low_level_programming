c2a065db06bb9201ac1e3dfbfc0b6d50565399fc
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-20 00:46:10 +0000

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog: frees the dog
+* @d: the dog to be freed
+**/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	free((*d).name);
+	free((*d).owner);
+	free(d);
+}
+}
