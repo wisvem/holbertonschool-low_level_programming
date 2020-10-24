71556690d4152fb53d766798c7222cd7de651906
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-24 17:00:28 +0000

    Betty style fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		if(separator != NULL && i < n-1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	va_end(parameters);
