125fdb93427126613631ec557603f844637ccecc
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-24 21:10:02 +0000

    Advance task

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,10 @@
-section .dta
-
-    message db "Hello, Holberton", 10
-
+section .data
+	message db "Hello, Holberton", 10
 section .text
-
-global _start
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 14
-    syscall
+global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 14
+	syscall
