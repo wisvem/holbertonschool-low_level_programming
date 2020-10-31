b0b61d62ef941a2ceeff9b7d0edb40816c1a42c3
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-31 03:57:02 +0000

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_list - frees a list
+* @head: head of the list
+**/
+void free_list(list_t *head)
+{
+	list_t *temp;
+	while(head != NULL)
+	{
+		temp = (*head).next;
+		free((*head).str);
+		free(head);
+		head = temp;
+	}
+	head = NULL;
+}
