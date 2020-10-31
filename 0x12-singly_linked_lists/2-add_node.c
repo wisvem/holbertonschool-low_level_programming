b15acbbf9dad47a45775911cb8939593fd3fa880
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-31 00:24:49 +0000

    Task 2 - Betty style

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,8 +2,8 @@
 
 /**
 * add_node - adds a new node at the beginning of a linked list
-* @h: head of the list
-* str: element to put in the new node
+* @head: head of the list
+* @str: element to put in the new node
 * Return: the address of the new element, or NULL if it failed
 **/
 list_t *add_node(list_t **head, const char *str)
