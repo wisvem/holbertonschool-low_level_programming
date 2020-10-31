4932c054cfb83a7c43f4066c2752174ce286a523
Wiston Venera Macias <2170@holbertonschool.com>
2020-10-31 02:04:19 +0000

    Task 3 - First commit with code

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,22 +1,41 @@
 #include "lists.h"
 
 /**
-* add_node_end - dds a new node at the end of a linked list
+* add_node_end - adds a new node at the end of a linked list
 * @head: head of the list
 * @str: element to put in the new node
 * Return: the address of the new element, or NULL if it failed
 **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *strclone = strdup(str);
-	int i = 0;	
-	list_t *newnode;
-	malloc(sizeof(list_t));
-	(*newnode).str = strdup(str);
-	while (str[i])
+	list_t *newnode = NULL;
+	int i = 0;
+	list_t *lastnode = *head;
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')/*Get size of str*/
 	{
 		i++;
 	}
+	(*newnode).str = strdup(str);
+	(*newnode).len = i;
+	(*newnode).next = NULL;
 	
-	return (*head);
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		while ((*lastnode).next != NULL)
+		{
+			lastnode = (*lastnode).next;
+		}
+		(*lastnode).next = newnode;
+	}
+	return (newnode);
 }
