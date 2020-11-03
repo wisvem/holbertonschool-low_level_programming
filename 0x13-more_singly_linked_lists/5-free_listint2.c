5d180d257385e646711fab47bf19e22900927bdc
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-03 23:27:58 +0000

    Task 5 - first attempt

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a list
+* @head: pointer to the list
+**/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *temp2;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		temp2 = (*temp).next;
+		free(temp);
+		temp = temp2;
+	}
+	*head = NULL;
+}
