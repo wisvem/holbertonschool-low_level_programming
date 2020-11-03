20af578571e4f35c0c9ccb00cee308bf2ed03c70
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-03 17:00:57 +0000

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* print_listint -prints all the elements of a list.
+* Return: the number of nodes
+* @h: head of the list
+**/
+size_t print_listint(const listint_t *h)
+{
+	int nodes;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (nodes = 0; h != NULL; nodes++)
+	{
+		printf("%i\n", (*h).n);
+		h = (*h).next;
+		nodes++;
+	}
+	return (nodes);
+}
