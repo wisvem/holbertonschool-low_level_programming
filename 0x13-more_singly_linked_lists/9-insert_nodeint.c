ccb2e259ecc9bb74a7de5c3a50b2d0b558b416c9
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-04 22:23:51 +0000

    Task 9 - Fixed everything

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newnode == NULL)
 		return (NULL);
 	(*newnode).n = n;
+	if (idx == 0)
+	{
+		(*newnode).next = *head;
+		*head = newnode;
+		return (newnode);
+	}
 	for (i = 0; i < idx; i++)
 	{
 		prev = current;
