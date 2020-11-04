1825cf7ae730ad9bd2cdbee4f7c012fa35eccca6
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-04 21:27:51 +0000

    Task 7 - Fixed non existing node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,18 +2,28 @@
 
 /**
 * get_nodeint_at_index - entry point
-* Return: the nth node of a linked list
+* Return: the nth node of a linked list,
+* NULL if the node does not exist
 * @head: head of the list
 * @index: position required
 **/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *node;
+	unsigned int i, list_size;
+	listint_t *node, *temp;
+
 
 	node = head;
 	if (node != NULL)
 	{
+		temp = head;
+		while (temp != NULL)
+		{
+			list_size++;
+			temp = (*temp).next;
+		}
+		if (index > list_size)
+			return (NULL);
 		for (i = 1; i <= index; i++)
 		{
 			node = (*node).next;
