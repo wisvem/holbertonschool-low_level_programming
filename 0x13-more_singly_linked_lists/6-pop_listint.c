3a0b2ef654375fe6374996580179d53ab643d7df
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-04 00:26:00 +0000

    Task 6 - first attempt

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* pop_listint - deletes the head node of a linked list
+* @head - pointer to the head
+* Return: the head node’s data (n)
+**/
+int pop_listint(listint_t **head)
+{
+	listint_t *temp, *temp2;
+	int n;
+
+	temp = *head;
+	n = (*temp).n;
+	temp2 = (*temp).next;
+	free(temp);
+	temp = temp2;
+	*head = temp;
+	return (n);
+}
