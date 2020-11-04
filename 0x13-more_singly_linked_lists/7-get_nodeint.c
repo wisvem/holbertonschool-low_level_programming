63c9a6415d31b9ba6ed32b5a2262084008b9772e
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-04 00:27:27 +0000

    Task 7 - first attempt

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - entry point
+* Return: the nth node of a linked list
+* @head: head of the list
+* @index: position required
+**/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *node;
+
+	node = head;
+	for (i = 1; i <= index; i++)
+	{
+		node = (*node).next;
+	}
+	return (node);
+}
