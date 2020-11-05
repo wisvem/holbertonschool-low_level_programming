061e6fb8fe020ca2ec07ba00c46aebea9c41c4f5
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-05 01:03:29 +0000

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+* pop_listint - deletes the head node of a linked list
+* @head: pointer to the head
+* Return: the head node’s data (n)
+**/
+int pop_listint(listint_t **head)
+{
+	listint_t *temp, *temp2;
+	int n;
+
+	temp = *head;
+	if (temp != NULL)
+	{
+		n = (*temp).n;
+		temp2 = (*temp).next;
+		free(temp);
+		temp = temp2;
+		*head = temp;
+	}
+	return (n);
+}
+
+/**
 * delete_nodeint_at_index - deletes the node at index of a linked list
 * @head: pointer to the head of the list
 * @index: index of the node that should be deleted
@@ -21,6 +43,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (list_size < index)
 		return (-1);
 	current = *head;
+	if (index == 0)
+	{
+		pop_listint(head);
+		return (1);
+	}
 	for (i = 0; i < index; i++)
 	{
 		prev = current;
