dca16dac383c33d2115be5a86d3ab736f322cdc8
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-05 04:12:35 +0000

    Task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* print_listint_safe - prints a linked list.
+* @head: head of the list
+* Return: the number of nodes in the list
+**/
+size_t print_listint_safe(const listint_t *head)
+{
+	int nodes;	
+	const listint_t *turtle = head;
+
+	if (head == NULL)
+	{
+		return (98);
+	}
+	printf("[%p] %i\n", (void *)head, (*head).n);
+	head = (*head).next;
+	for (nodes = 0; head != NULL; nodes++)
+	{
+		if (head < turtle)
+		{
+			printf("[%p] %i\n", (void *)head, (*head).n);
+		}
+		else
+		{
+			printf("-> [%p] %i\n", (void *)head, (*head).n);
+			break;
+		}
+		head = (*head).next;
+		turtle = (*turtle).next;
+	}
+	return (nodes);
+}
