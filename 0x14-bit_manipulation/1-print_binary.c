5d91b2d840b29b51972743d510b165d88ed2313e
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-05 23:59:04 +0000

    Task 1 - Added putchar

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,44 +1,18 @@
 #include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
 * print_binary - print binary representation
 * @n: decimal to print
 **/
 void print_binary(unsigned long int n)
 {
-	unsigned long int x = n;
-	int res, i, j, count = 0;
-	char ptr[1024];
-	unsigned long int  y = x;
-
-	if (x < 1)
+	if (n != 0)
 	{
-		_putchar('0' + 0);
-		return;
+		print_binary(n >> 1);
+		_putchar('0' + (n & 1));
 	}
-	for (j = 0; y > 0; j++)
-		y = y / 2;
-	for (i = 0; x > 0; i++)
+	else
 	{
-		res = x % 2;
-		x = x / 2;
-		ptr[i] = res;
-		count++;
-	}
-	for (; i > 0; i--)
-	{
-		_putchar('0' + ptr[i - 1]);
+		_putchar('0');
 	}
 }
