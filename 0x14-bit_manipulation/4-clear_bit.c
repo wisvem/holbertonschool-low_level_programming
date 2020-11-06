c8eaaba26d3496f9a3f1f1ce99706b7b6337001e
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-06 00:41:01 +0000

    Task 4 - Betty style

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,11 +2,11 @@
 #include "holberton.h"
 
 /**
- * clear_bit - sets the value of a bit to 0 at a given index
- * @n: pointer to a number
- * @index: given index
- * Return: 1 if it worked, or -1 if an error occurred
- */
+* clear_bit - sets the value of a bit to 0 at a given index
+* @n: pointer to a number
+* @index: given index
+* Return: 1 if it worked, or -1 if an error occurred
+**/
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (!n || index >= sizeof(*n) * 8)
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* flip_bits - returns the number of bits 
+* you would need to flip to get from one number to another.
+* @n: number 1
+* @m: number 2
+* Return: the number of the bits
+**/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	n = n ^ m;
+	m = 0;
+	while (n != 0)
+	{
+		m = m + 1 & n;
+		n >>= 1;
+	}
+	return (m);
+}
