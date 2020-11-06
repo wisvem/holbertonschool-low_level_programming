454fd668bf952bba0192bc06c691f6bc84b9ddad
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-06 00:22:54 +0000

    Task 2 - Betty style

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,6 +4,7 @@
 * get_bit - returns the value of a bit at a given index
 * @index: given index
 * @n: decimal number
+* Return: the value of the bit at index or -1 if an error occured
 **/
 int get_bit(unsigned long int n, unsigned int index)
 {
