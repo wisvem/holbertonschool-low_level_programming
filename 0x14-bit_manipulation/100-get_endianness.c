e090a1a67c8b31975c24a8e8359728d324084091
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-06 01:22:15 +0000

    Task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
-* get_endianess - checks the endianness
+* get_endianness - checks the endianness
 * Return: 0 if big endian, 1 if little endian
 **/
 int get_endianness(void)
 {
 	int x = 1;
-	x = *(char *)(&x) == 1;
 
+	x = *(char *)(&x) == 1;
 	return (x);
 }
