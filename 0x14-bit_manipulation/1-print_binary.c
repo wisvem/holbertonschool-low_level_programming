1f626e2aba1912be077a32fb01655847f0961298
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-06 00:11:43 +0000

    Task 1 - fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,13 +6,15 @@
 **/
 void print_binary(unsigned long int n)
 {
-	if (n != 0)
+	if (n > 1)
 	{
 		print_binary(n >> 1);
 		_putchar('0' + (n & 1));
 	}
-	else
+	else if (n == 1)
 	{
-		_putchar('0');
+		_putchar('1');
 	}
+	else
+		_putchar('0');
 }
