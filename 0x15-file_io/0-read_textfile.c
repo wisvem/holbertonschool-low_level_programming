57c82ae77246625a99fc64338a606932c75dff3b
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-09 17:18:10 +0000

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: text file to be read
+* @letters: number of letters it should read and print
+* Return: the actual number of letters it could read and print
+* 0, if the file can not be opened or read, if filename is NULL
+* if write fails or does not write the expected amount of bytes
+**/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_d = 0, read_r;
+	char *buff = malloc(sizeof(char) * letters);
+
+	if (buff == NULL || filename == NULL || letters == 0)
+		return (file_d);
+
+	file_d = open(filename, O_RDONLY);
+	if (file_d == -1)
+		return (0);
+	read_r = read(file_d, buff, letters);
+	if (read_r == 0)
+		return (read_r);
+
+	buff[letters] = '\0';
+
+	close(file_d);
+
+	return (file_d);
+}
