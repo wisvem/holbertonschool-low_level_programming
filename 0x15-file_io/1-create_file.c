0adf3497a04fa137f2ecce9fef045f49fcff9128
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-09 21:12:42 +0000

    Task 1 - change POSIX

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[text_size] != '\0')
 			text_size++;
-		write_r = write(file_D, text_content, text_size);
+		write_r = write(file_d, text_content, text_size);
 	}
 	if (write_r < 0)
 		return (-1);
