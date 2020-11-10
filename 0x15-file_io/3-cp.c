58916a0c18604d465238205f0dfdeff6adc72320
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-10 03:22:57 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 	char *from_name, *to_name, buff[1024];
-	int file_d, file_s, read_r, write_r;
+	int f_dest, f_source, read_r, write_r;
 
 	if (argc != 3)
 	{
@@ -17,35 +17,39 @@ int main(int argc, char *argv[])
 	}
 	from_name = argv[1];
 	to_name =  argv[2];
-	file_d = open(to_name, O_RDWR | O_CREAT | O_TRUNC, 0664);
-	if (file_d == -1)
+	f_dest = open(to_name, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (f_dest == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", to_name), exit(99);
+		dprintf(STDERR_FILENO, "Can't 1 write to %s\n", to_name), exit(99);
 	}
-	file_s = open(from_name, O_RDONLY | 0600);
-	read_r = read(file_s, buff, 1024);
-	if (file_s == -1 || read_r == -1)
+	f_source = open(from_name, O_RDONLY);
+	read_r = read(f_source, buff, 1024);
+	if (f_source == -1 || read_r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name),
+		dprintf(STDERR_FILENO, "Error: Can't 1 read from file %s\n", from_name),
 			exit(98);
 	}
 	while (read_r > 0)
 	{
-		write_r = write(file_d, buff, read_r);
+		write_r = write(f_dest, buff, read_r);
 		if (write_r == -1)
 		{
-			dprintf(STDERR_FILENO, "Can't write to %s\n", to_name), exit(99);
+			dprintf(STDERR_FILENO, "Can't 2 write to %s\n", to_name), exit(99);
 		}
-		read_r = write(file_s, buff, read_r);
-		if (read_r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_name),
-				exit(98);
-		}
+		read_r = write(f_source, buff, 1024);
+/*
+*		printf("Despues %d\n", read_r);
+*
+*		if (read_r == -1)
+*		{
+*			dprintf(STDERR_FILENO, "Error: Can't 2 read from file %s\n", from_name),
+*				exit(98);
+*		}
+*/
 	}
-	if (close(file_d) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", to_name);
-	if (close(file_s) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close %s\n", from_name);
+	if (close(f_dest) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close %s\n", to_name), exit(100);
+	if (close(f_source) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close %s\n", from_name), exit(100);
 	return (0);
 }
