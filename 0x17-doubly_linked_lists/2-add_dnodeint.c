f69deb7fac966340580644a67d4aa589887b5286
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-13 14:57:03 +0000

    Task 2 - check if the list exists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,13 +10,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
 
-	newnode = malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
+	if (head != NULL)
 	{
-		return (NULL);
+		newnode = malloc(sizeof(dlistint_t));
+		if (newnode == NULL)
+		{
+			return (NULL);
+		}
+		(*newnode).n = n;
+		(*newnode).next = *head;
+		*head = newnode;
+		return (newnode);
 	}
-	(*newnode).n = n;
-	(*newnode).next = *head;
-	*head = newnode;
-	return (newnode);
+	return (NULL);
 }
