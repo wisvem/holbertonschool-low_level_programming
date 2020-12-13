89891ee54efda881832f3ed071c8d76a2c25fbaa
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-13 23:11:51 +0000

    Task 8 - Handled index equals to size list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -42,6 +42,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		temp = (*temp).next;
 		list_size++;
 	}
+	if (list_size == index)
+		return (-1);
 	if (list_size < index)
 		return (-1);
 	current = *head;
