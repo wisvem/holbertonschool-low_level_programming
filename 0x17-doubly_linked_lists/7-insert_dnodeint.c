d00b3e508154eafc0aaf00cc37f09be1fa1e508f
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-13 16:57:48 +0000

    Task 7 - prev included

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,6 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = (*current).next;
 	}
 	(*prev).next = newnode;
+	(*current).prev = newnode;
 	(*newnode).next = current;
 	(*newnode).prev = prev;
 	return (newnode);
