276f3e780b7811112e29581091eb20521c1e4188
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-13 16:13:35 +0000

    Task 2 - prev included

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,8 +16,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	(*newnode).n = n;
-	(*newnode).next = *head;
 	(*newnode).prev = NULL;
+	(*newnode).next = *head;
+	if (*head != NULL)
+	{
+		(**head).prev = newnode;
+	}
 	*head = newnode;
 	return (newnode);
 }
