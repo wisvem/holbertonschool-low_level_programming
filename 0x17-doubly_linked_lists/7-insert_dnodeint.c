32076225780478f5ecd68fbe85d12ec77f34529d
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-14 02:47:22 +0000

    Task 7 - Alejandro Test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,40 +9,41 @@
 **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *prev, *current, *newnode, *temp;
-	unsigned int i;
-	unsigned int list_size = 0;
+	dlistint_t *auxiliar_node = NULL, *new_node = NULL, *temp;
+	unsigned int i = 0, list_size = 0;
 
-	temp = *h;
-	while (temp != NULL)
+	if (h)
 	{
-		temp = (*temp).next;
-		list_size++;
+		new_node = malloc(sizeof(dlistint_t));
+		if (!new_node)
+			return (NULL);
+		temp = *h;
+		while (temp != NULL)
+		{
+			temp = (*temp).next;
+			list_size++;
+		}
+		if (list_size == idx)
+			return (add_dnodeint_end(h, n));
+		if (list_size < idx)
+			return (NULL);
+		if (idx == 0)
+			return (add_dnodeint(h, n));
+
+		auxiliar_node = *h;
+
+		for (; i < idx; i++)
+		{
+			if (!(*auxiliar_node).next)
+				return (NULL);
+			auxiliar_node = (*auxiliar_node).next;
+		}
+		(*new_node).n = n;
+		(*new_node).prev = (*auxiliar_node).prev;
+		(*(*auxiliar_node).prev).next = new_node;
+		(*auxiliar_node).prev = new_node;
+		if ((*auxiliar_node).next)
+			(*new_node).next = auxiliar_node;
 	}
-	if (list_size == idx)
-	{
-		return (add_dnodeint_end(h, n));
-	}
-	if (list_size < idx)
-		return (NULL);
-	current = *h;
-	if (idx == 0)
-	{
-		/*Add node to head*/
-		return (add_dnodeint(h, n));
-	}
-	for (i = 0; i < idx; i++)
-	{
-		prev = current;
-		current = (*current).next;
-	}
-	newnode = malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
-		return (NULL);
-	(*newnode).n = n;
-	(*newnode).next = current;
-	(*newnode).prev = prev;
-	(*prev).next = newnode;
-	(*current).prev = newnode;
-	return (newnode);
+	return (EXIT_SUCCESS);
 }
