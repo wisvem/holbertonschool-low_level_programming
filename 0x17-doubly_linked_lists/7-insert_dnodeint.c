cc4ebc8cbe48985133956999262f938e1df1fd30
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-14 03:12:00 +0000

    Task 7 - Alejandro Test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - Entry point
+* @h: head of the list
+* Return: the number of elements in a linked list
+**/
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t count = 0;
+
+        while (h != NULL)
+        {
+                h = (*h).next;
+                count++;
+        }
+        return (count);
+}
+/**
 * insert_dnodeint_at_index - inserts a new node at a given position
 * @h: pointer to head
 * @idx: the given position
