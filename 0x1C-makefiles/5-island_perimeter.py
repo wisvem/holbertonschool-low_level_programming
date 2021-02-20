822f0abde4bc1dde1ed5a6bec91e5eea190984c9
Wiston Venera Macías <wisvem@hotmail.com>
2021-02-19 19:55:39 -0500

    Task 6 - Fixed vertical search

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,19 +12,18 @@ def island_perimeter(grid):
         for n in row:
             if n is 1:
                 count += 1
-
         for j in range(1, len(row)):
             if row[j] is 1:
+                print(row[j] is row[j-1])
                 if row[j] is row[j-1]:
                     hunions += 1
 
-    for row in grid:
-        for j in range(1, len(row)):
-            if row[j] is 1:
-                if row[j] is row[j-1]:
-                    hunions += 1
+    for index, row in enumerate(grid):
+        for j in range(len(row)):
+            if row[j] is 1 and index < len(grid)-1:
+                if row[j] is grid[index+1][j]:
+                    vunions += 1
 
-#    print(hunions, vunions, count)
     count = count * 4
     unions = (vunions+hunions)*2
     count = count - unions
