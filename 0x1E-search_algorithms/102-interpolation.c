cfdc2a1f80f0990c8d1728982f5492a124782244
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-20 05:31:19 +0000

    Task 9

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,5 +10,44 @@
 **/
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t mid;
+
+	if (array != NULL)
+	{
+		while ((array[high] != array[low]) &&
+			   (value >= array[low]) && (value <= array[high]))
+		{
+			mid = low + (((double)(high - low) /
+						  (array[high] - array[low])) *
+						 (value - array[low]));
+			if (array[mid] < value)
+			{
+				printf("Value checked array[%lu] = [%i]\n", mid, array[mid]);
+				low = mid + 1;
+			}
+			else if (value < array[mid])
+			{
+				printf("Value checked array[%lu] = [%i]\n", mid, array[mid]);
+				high = mid - 1;
+			}
+			else
+			{
+				printf("Value checked array[%lu] = [%i]\n", mid, array[mid]);
+				return (mid);
+			}
+		}
+
+		if (value == array[low])
+		{
+			printf("Value checked array[%lu] = [%i]\n", low, array[low]);
+			return (low);
+		}
+		mid = low + (((double)(high - low) /
+					  (array[high] - array[low])) *
+					 (value - array[low]));
+		printf("Value checked array[%lu] is out of range\n", mid);
+	}
+	return (-1);
 }
