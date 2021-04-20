72edb44ba0e3b3f996e13c203fc0d07fa036a553
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-20 01:22:34 +0000

    Task 1 - Added a condition when array is null

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,25 +12,28 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t l = 0, r = size - 1, m, i;
 
-	while (l <= r)
+	if (array != NULL)
 	{
-		printf("Searching in array: ");
-		for (i = l; i < r; i++)
+		while (l <= r)
 		{
-			printf("%i, ", array[i]);
+			printf("Searching in array: ");
+			for (i = l; i < r; i++)
+			{
+				printf("%i, ", array[i]);
+			}
+			printf("%i\n", array[i]);
+			m = (l + r) / 2;
+			if (array[m] < value)
+			{
+				l = m + 1;
+			}
+			if (array[m] > value)
+			{
+				r = m - 1;
+			}
+			if (array[m] == value)
+				return (m);
 		}
-		printf("%i\n", array[i]);
-		m = (l + r) / 2;
-		if (array[m] < value)
-		{
-			l = m + 1;
-		}
-		if (array[m] > value)
-		{
-			r = m - 1;
-		}
-		if (array[m] == value)
-			return (m);
 	}
 	return (-1);
 }
