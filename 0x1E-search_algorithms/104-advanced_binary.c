bb1fcb47f456a8f203083c86f5d467c994b43006
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-20 10:05:36 +0000

    Fixed break condition of the recursive function

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -6,17 +6,19 @@
 * @l: left limit
 * @r: right limit
 **/
-void print_array(int *array, size_t l, size_t r)
+void print_array(int *array, size_t l, size_t r, size_t size)
 {
 	size_t i;
 
+	/*printf("l: %lu r: %lu\n", l, r);*/
 	printf("Searching in array: ");
-	for (i = l; i < r; i++)
+	for (i = l; i < (r < size ? r : size); i++)
 	{
 		printf("%i, ", array[i]);
 	}
 	printf("%i\n", array[i]);
 }
+
 /**
 * bs - searches for a value in an array of integers
 * @array: pointer to the first element of the array to search in
@@ -26,27 +28,26 @@ void print_array(int *array, size_t l, size_t r)
 * Return: the first index where value is located \
 * or -1 if value is not present in array or if array is NULL
 **/
-int bs(int *array, size_t l, size_t r, int value)
+int bs(int *array, size_t l, size_t r, int value, size_t size)
 {
 	size_t mid;
 
 	/*printf("l: %lu r: %lu\n", l, r);*/
-	if (r >= l)
+	if (r > l)
 	{
-
-		print_array(array, l, r);
+		print_array(array, l, r, size);
 		mid = (l + r) / 2;
 		if (array[mid] == value)
 		{
-			return (bs(array, l, mid - 1, value));
+			return (bs(array, l, mid - 1, value, size));
 		}
 		else if (value < array[mid])
 		{
-			return (bs(array, l, mid - 1, value));
+			return (bs(array, l, mid - 1, value, size));
 		}
 		else
 		{
-			return (bs(array, mid + 1, r, value));
+			return (bs(array, mid + 1, r, value, size));
 		}
 	}
 	else if (value == array[l])
@@ -69,12 +70,7 @@ int advanced_binary(int *array, size_t size, int value)
 
 	if (array != NULL)
 	{
-		if (array[0] == value)
-		{
-			print_array(array, l, r);
-			return (0);
-		}
-		return (bs(array, l, r, value));
+		return (bs(array, l, r, value, size));
 	}
 	return (-1);
 }
