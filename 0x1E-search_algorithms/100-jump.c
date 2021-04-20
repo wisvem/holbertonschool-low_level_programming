bb9a78f7c5e08bef3ea835b48adfa6cdc92b9614
Wiston Venera Macías <wisvem@hotmail.com>
2021-04-20 03:35:28 +0000

    Added a condition to avoid duplicated print

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,15 +11,17 @@
 **/
 int jump_search(int *array, size_t size, int value)
 {
-	size_t start = 0, block;
+	size_t start = 0, block = 0;
 	size_t end, i = 0;
 
 	if (array != NULL)
 	{
-		block = sqrt(size);
 		end = block;
+		block = sqrt(size);
 		while (array[end] < value && end < size)
 		{
+			if (array[block] >= value)
+				break;
 			printf("Value checked array[%lu] = [%i]\n", start, array[start]);
 			start = end;
 			end = end + block;
--- 0x1E-search_algorithms/100-main.c
+++ 0x1E-search_algorithms/100-main.c
@@ -10,8 +10,9 @@
 int main(void)
 {
 	int array[] = {
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		1};
 	size_t size = sizeof(array) / sizeof(array[0]);
+	printf("size %lu\n", size);
 
 	printf("Found %d at index: %d\n\n", 6, jump_search(array, size, 6));
 	printf("Found %d at index: %d\n\n", 1, jump_search(array, size, 1));
